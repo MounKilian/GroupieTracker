fcafae280f84eaf55e84696164dbbbd02a118a26
mkilian <kilian.moun@ynov.com>
2024-04-12 15:32:20 +0200

    add websocket server and start room

--- function/server.go
+++ function/server.go
@@ -2,10 +2,15 @@ package groupieTracker
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 func Server() {
+	room := NewRoom()
+	go room.Start()
 	var currentUser User
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +20,69 @@ func Server() {
 		currentUser = Formulaire(w, r)
 		fmt.Println(currentUser)
 	})
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		HandleWebSocket(room, w, r)
+	})
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe(":8080", nil)
+}
+
+type Room struct {
+	id         string
+	clients    map[*websocket.Conn]bool
+	register   chan *websocket.Conn
+	unregister chan *websocket.Conn
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func NewRoom() *Room {
+	return &Room{
+		id:         "edi",
+		clients:    make(map[*websocket.Conn]bool),
+		register:   make(chan *websocket.Conn),
+		unregister: make(chan *websocket.Conn),
+	}
+}
+
+func (room *Room) Start() {
+	if room.id == "edi" {
+		for {
+			select {
+			case conn := <-room.register:
+				room.clients[conn] = true
+				log.Println("Client connected")
+			case conn := <-room.unregister:
+				delete(room.clients, conn)
+				log.Println("Client disconnected")
+			}
+		}
+	}
+}
+
+func HandleWebSocket(room *Room, w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer ws.Close()
+
+	if room.id == "edi" {
+		room.register <- ws
+		defer func() { room.unregister <- ws }()
+
+		for {
+			_, _, err := ws.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				break
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module groupieTracker
 
 go 1.22.2
 
-require github.com/mattn/go-sqlite3 v1.14.22 // indirect
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)

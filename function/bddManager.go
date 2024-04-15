4106104a20e7f4f7d88b3214eef2049d8314cbac
catom <tom.calvo@ynov.com>
2024-04-15 15:02:00 +0200

    add verification on room creation

--- function/bddManager.go
+++ function/bddManager.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -72,18 +73,33 @@ func resetUserTable(db *sql.DB) {
 }
 
 func createNewRoom(db *sql.DB, value [4]string) {
-	insertQuery := "INSERT INTO ROOMS (id, created_by, max_player, name, id_game) VALUES (?, ?, ?, ?, ?)"
-	_, err := db.Exec(insertQuery, nil, value[0], value[1], value[2], value[3])
-	if err != nil {
-		log.Fatal(err)
+	//Convert to int
+	nbMaxPlayer, _ := strconv.Atoi(value[1])
+	created_by, _ := strconv.Atoi(value[0])
+	id_game, _ := strconv.Atoi(value[3])
+	if nbMaxPlayer > 6 {
+		log.Println("to much player")
+	} else {
+		insertQuery := "INSERT INTO ROOMS (id, created_by, max_player, name, id_game) VALUES (?, ?, ?, ?, ?)"
+		_, err := db.Exec(insertQuery, nil, created_by, nbMaxPlayer, value[2], id_game)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		currentUserValue := [2]int{getRoomFromUser(db, created_by), created_by}
+		addPlayer(db, currentUserValue)
 	}
 }
 
 func addPlayer(db *sql.DB, value [2]int) {
-	insertQuery := "INSERT INTO ROOMS (id_room, id_user, score) VALUES (?, ?, ?)"
-	_, err := db.Exec(insertQuery, value[0], value[1], 0)
-	if err != nil {
-		log.Fatal(err)
+	if checkNbPlayer(db, value[0]) > getMaxPlayer(db, value[0]) {
+		log.Println("Party already full")
+	} else {
+		insertQuery := "INSERT INTO ROOMS (id_room, id_user, score) VALUES (?, ?, ?)"
+		_, err := db.Exec(insertQuery, value[0], value[1], 0)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
@@ -93,4 +109,36 @@ func updatePlayerScore(db *sql.DB, id_room int, score User) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func checkNbPlayer(db *sql.DB, id_room int) int {
+	var nbPLayer int
+	query := "SELECT COUNT(*) FROM ROOM_USERS WHERE id = ?"
+	err := db.QueryRow(query, id_room).Scan(&nbPLayer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nbPLayer
+}
+
+func getMaxPlayer(db *sql.DB, id_room int) int {
+	var nbMaxPlayer int
+
+	query := "SELECT `max_player` FROM ROOMS WHERE id = ?"
+	err := db.QueryRow(query, id_room).Scan(&nbMaxPlayer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nbMaxPlayer
+}
+
+func getRoomFromUser(db *sql.DB, id_user int) int {
+	var idRoom int
+
+	query := "SELECT `id` FROM ROOMS WHERE created_by = ?"
+	err := db.QueryRow(query, id_user).Scan(&idRoom)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return idRoom
 }

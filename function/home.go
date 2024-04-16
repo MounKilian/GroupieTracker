348c7e4738ad6015cb586558e554aef9a113166a
mkilian <kilian.moun@ynov.com>
2024-04-16 17:10:07 +0200

    fix merge

--- function/home.go
+++ function/home.go
@@ -6,10 +6,10 @@ import (
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request, errorMessage Error) {
+func Home(w http.ResponseWriter, r *http.Request) {
 	template, err := template.ParseFiles("./index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	template.Execute(w, errorMessage)
+	template.Execute(w, nil)
 }
